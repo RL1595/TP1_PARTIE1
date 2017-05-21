98142924e76d63684c82aa848106fa34005a8f2a
piotte13 <jeremie.piotte@gmail.com>
2017-05-21 14:07:11 -0400

    Premier commit

--- TP1_partie1/main.c
+++ TP1_partie1/main.c
@@ -1,15 +1,54 @@
-//
-//  main.c
-//  TP1_partie1
-//
-//  Created by Roxane Lupien on 17-05-21.
-//  Copyright © 2017 Roxane Lupien. All rights reserved.
-//
+/*
+ *Travail pratique 1, partie 1
+ *@Roxane Lupien
+ *@18/05/17
+ *Dernière modification le 18/05/17
+ */
 
+//Commandes pré-processeur
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+//Prototypes
+int valeur_aleatoire(int min, int max);
+
+//Programme principal
+int main(void){
+    
+    //Déclaration des variables
+    minimum = 3;
+    maximum = 7;
+    
+    resultat = valeur_aleatoire(minimum, maximum);
+    afficher_resultats(resultat);
+    
+    return EXIT_SUCCESS;
 }
+
+int valeur_aleatoire(int min, int max){
+    
+    //Déclaration des variables du sous-programme
+    int resultat;
+    
+    do{
+        srand(time(NULL));
+        resultat = (rand() % (max + 1 - min)) + min;
+    }
+    while(resultat != 3 || resultat != 7)
+        
+        scanf("%d", &resultat)
+        
+        return resultat;
+}
+
+void afficher_resultats(resultat){
+    
+    if(resultat == 3){
+        printf("pile");
+    }
+    else if(resultat == 7){
+        printf("face");
+    }
+}
+
