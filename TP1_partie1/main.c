aaf79d9174c08df716a5a40e583920d5a1a0e3b7
piotte13 <jeremie.piotte@gmail.com>
2017-05-21 13:54:49 -0400

    Initial Commit

---
+++ TP1_partie1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TP1_partie1
+//
+//  Created by Roxane Lupien on 17-05-21.
+//  Copyright © 2017 Roxane Lupien. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
